cf62fb5d72e130b3a87f9a1570e8227bf617b3d2
WL777777 <wallace.lins19@gmail.com>
2023-03-02 22:31:37 -0300

    Aula 1

---
+++ app/Http/Controllers/ReclamacaoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ReclamacaoModel;
+use Illuminate\Http\Request;
+
+class ReclamacaoModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ReclamacaoModel  $reclamacaoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReclamacaoModel $reclamacaoModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ReclamacaoModel  $reclamacaoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReclamacaoModel $reclamacaoModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ReclamacaoModel  $reclamacaoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReclamacaoModel $reclamacaoModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ReclamacaoModel  $reclamacaoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReclamacaoModel $reclamacaoModel)
+    {
+        //
+    }
+}
---
+++ resources/views/header.php
@@ -0,0 +1,17 @@
+
+<head>
+    <link href = "./public/css/header" rel="stylesheet"> 
+</head>
+
+
+
+    <header>
+      <nav>
+       <ul class="nav-list">
+            <a href="/Principal">Home</a>
+            <a href="/QuemSomos">QuemSomos</a>
+            <a href="/Reclamacao">Reclamação</a>
+            <a href="/Contato">Contato</a>
+        </ul>
+      </nav>
+    </header>
